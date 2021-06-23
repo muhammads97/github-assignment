d5c2f8e2108243920fb0d5f7048acd562d0546c4
Muhammad <msalah.29.10@gmail.com>
2021-06-23 20:10:57 +0200

    change home nav props

--- src/navigation/home-stack-navigator/home-stack-navogator.types.ts
+++ src/navigation/home-stack-navigator/home-stack-navogator.types.ts
@@ -1,8 +1,7 @@
 import { CompositeNavigationProp, RouteProp } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
-import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs"
 import { AppRoute } from "@github/navigation/routes"
-import { ITabParamList } from "@github/navigation/bottom-tab-navigator"
+import { ITabNavigationProp } from "@github/navigation/bottom-tab-navigator"
 
 export type IHomeParamList = {
   [AppRoute.Home]: {}
@@ -14,6 +13,6 @@ type IHomeRoute = keyof IHomeParamList
 // export type IHomeNavigationProp<R extends IHomeRoute> = StackNavigationProp<IHomeParamList, R>
 export type IHomeNavigationProp<R extends IHomeRoute> = CompositeNavigationProp<
   StackNavigationProp<IHomeParamList, R>,
-  BottomTabNavigationProp<ITabParamList, AppRoute.Home>
+  ITabNavigationProp<AppRoute.Home>
 >
 export type IHomeRoutProp<R extends IHomeRoute> = RouteProp<IHomeParamList, R>
